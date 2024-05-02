d25ebd6edb51aea7bcf567321c509ff9a2853bd8
pravalikap23 <saipravallikap58@gmail.com>
2024-05-02 16:07:35 +0100

    pong

--- src/App.java
+++ src/App.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 
 public class App {
     public static void main (String[] args){
-        JFrame window = new JFrame("Ping Pongy");
+        JFrame window = new JFrame("Ping Pong");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         Board board = new Board();
